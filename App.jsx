8a7fcc77bfc5323be936108aaa42cdb3b49c86ca
Maheswarisathishkumar <maheswari961224@gmail.com>
2025-03-12 16:11:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+// import Navbar from "./Layouts/Navbar";
+// import Hero from "./Layouts/Hero";
+// import Card from "./Layouts/Card";
+// import Login from "./Components/login";
+import Products from "./Components/products";
+import './App.css'
+
+
+const App = () => {
+  return (
+<>
+{/* <Navbar/>
+<Hero/>
+<Card/>
+<Login/> */}
+<Products/>
+
+
+</>
+  )
+}
+
+
+export default App;
+
+
