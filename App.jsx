275b8dbb9f5d0a80b792205e208457d67b6dcc07
Maheswarisathishkumar <maheswari961224@gmail.com>
2025-03-12 16:59:32 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,19 @@
 // import Navbar from "./Layouts/Navbar";
 // import Hero from "./Layouts/Hero";
 // import Card from "./Layouts/Card";
-// import Login from "./Components/login";
-import Products from "./Components/products";
+ import Login from "./Components/login";
+// import Products from "./Components/products";
 import './App.css'
 
 
 const App = () => {
   return (
 <>
-{/* <Navbar/>
+{/*  <Navbar/>
 <Hero/>
-<Card/>
-<Login/> */}
-<Products/>
+<Card/> */}
+<Login/> 
+{/* <Products/> */}
 
 
 </>
